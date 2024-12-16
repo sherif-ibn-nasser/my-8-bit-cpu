5978b3be91c0eb16d6d04d8e3dfed61c989569f6
sherif2003 <sherif2102003@gmail.com>
2024-12-16 16:21:19 +0200

    Write the core
    - Update the reset logic of the register file

--- verilog/reg_file.v
+++ verilog/reg_file.v
@@ -10,8 +10,19 @@ module reg_file (
         BL = 1,
         CL = 2,
         DL = 3;
+
+    always @* begin
+        if (reg_r) // Read is anabled
+            case (reg_r_select[3:0])
+            AL: reg_r_line = al;
+            BL: reg_r_line = bl;
+            CL: reg_r_line = cl;
+            DL: reg_r_line = dl;
+            endcase
+        else reg_r_line = 0;
+    end
     
-    always @(posedge clk) begin
+    always @(posedge clk or posedge reset) begin
         if (reset) begin
             al = 0;
             bl = 0;
@@ -28,14 +39,4 @@ module reg_file (
         end
     end
 
-    always @* begin
-        if (reg_r) // Read is anabled
-            case (reg_r_select[3:0])
-            AL: reg_r_line = al;
-            BL: reg_r_line = bl;
-            CL: reg_r_line = cl;
-            DL: reg_r_line = dl;
-            endcase
-        else reg_r_line = 0;
-    end
 endmodule
